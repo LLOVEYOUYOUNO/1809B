70003a8049fbf0d8db3c12c6eba8bf7968479743
taofu <taofujob@gmail.com>
2019-05-08 22:40:45 +0800

    Initial commit

---
+++ app/src/main/java/com/jy/small/training/MainActivity.java
@@ -0,0 +1,51 @@
+package com.jy.small.training;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import jy.com.libbdmap.training.BdMapActivity;
+import jy.com.libumengsharelogin.LoginActivity;
+import jy.com.libumengsharelogin.ShareActivity;
+
+/*
+ * created by taofu on 2019/5/5
+ **/
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    public void onClick(View v){
+        switch (v.getId()){
+
+            case R.id.btn_start_umeng_share:{
+                Intent intent = new Intent(this,ShareActivity.class);
+                startActivity(intent);
+
+                int i = 10 / 0;
+                break;
+            }
+
+            case R.id.btn_start_umeng_login:{
+                Intent intent = new Intent(this,LoginActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.btn_start_bdmap:{
+                Intent intent = new Intent(this,BdMapActivity.class);
+                startActivity(intent);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/jy/small/training/MyApplication.java
@@ -0,0 +1,32 @@
+package com.jy.small.training;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.jy.small.training.utils.Logger;
+import com.umeng.commonsdk.UMConfigure;
+import com.umeng.socialize.PlatformConfig;
+import jy.com.libbdmap.BdMapUtils;
+import jy.com.libumengsharelogin.MyUmengUtils;
+
+/*
+ * created by taofu on 2019/5/7
+ **/
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        MyUmengUtils.initUmeng(this);
+
+        BdMapUtils.init(this);
+    }
+
+
+
+
+
+}
