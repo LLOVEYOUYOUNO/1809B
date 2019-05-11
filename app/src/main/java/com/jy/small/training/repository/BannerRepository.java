cf6601150a1e0c5367658d69735fac0f59be6833
taofu <taofujob@gmail.com>
2019-05-11 09:51:05 +0800

    环信集成

--- app/src/main/java/com/jy/small/training/repository/BannerRepository.java
+++ app/src/main/java/com/jy/small/training/repository/BannerRepository.java
@@ -9,7 +9,10 @@ import com.jy.small.training.repository.remote.ok.DataService;
 import java.util.List;
 
 import com.trello.rxlifecycle2.android.ActivityEvent;
+import com.trello.rxlifecycle2.android.FragmentEvent;
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
+import com.trello.rxlifecycle2.components.support.RxFragment;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
@@ -26,8 +29,7 @@ public class BannerRepository implements BannerConstract.IBannerSource {
 
 
     @Override
-    public void getBanner(RxAppCompatActivity activity,final IBaseCallBack<List<Banner>> callBack) {
-
+    public void getBanner(RxAppCompatActivity activity, final IBaseCallBack<List<Banner>> callBack) {
 
         DataService.getService().getBanners().flatMap(new Function<HttpResult<List<Banner>>, ObservableSource<List<Banner>>>() {
             @Override
@@ -41,7 +43,7 @@ public class BannerRepository implements BannerConstract.IBannerSource {
         }).subscribeOn(Schedulers.io())
                 .unsubscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .compose(activity.<List<Banner>>bindUntilEvent(ActivityEvent.DESTROY)) // 但我们view 层的 activity 执行了 destroy 生命周期方法，自动解除订阅关系
+                .compose(activity.<List<Banner>>bindUntilEvent(ActivityEvent.DESTROY)) // 当我们view 层的 activity 执行了 destroy 生命周期方法，自动解除订阅关系，并且把 ok 的 socket 断开
                 .subscribe(new Observer<List<Banner>>() {
             @Override
             public void onSubscribe(Disposable d) {
